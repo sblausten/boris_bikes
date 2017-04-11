3ab9130fdd34467e08c5848ca84c60e38a06d1eb
Sam <sblausten@Sams-Mac.local>
2017-04-11 13:39:48 +0100

    Step 11 - incomplete tests

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -2,8 +2,22 @@ require './lib/Bike'
 
 class DockingStation
 
+	def initialize
+		@bike_count = 0
+	end
+
 	def release_bike
-		Bike.new
+		if @bike_count >= 1
+			Bike.new
+			@bike_count -= 1
+		elsif @bike_count < 1
+			"No bikes! Take the bus mate."
+		end
+	end
+
+	def dock_bike
+		@bike_count += 1
+		true
 	end
 
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,12 +1,38 @@
 require 'DockingStation'
 
 describe DockingStation do
+ {DockingStation.new}
+
+	it "a) gets a bike, and then b) expects the bike to be working" do
+		expect(subject.release_bike.working?).to eq(true)
+	end
+
+	it "docks a bike at a docking station" do
+		expect(subject.release_bike.working?).to eq(true)
+	end
+
+	it "docks a bike at a docking station" do
+		expect(subject.release_bike.working?).to eq(true)
+	end
+
+	it { expect(subject).to respond_to(:release_bike)}
+
+end
+
+describe DockingStation do
 	subject {DockingStation.new}
 
 	it "a) gets a bike, and then b) expects the bike to be working" do
 		expect(subject.release_bike.working?).to eq(true)
 	end
 
+	it "docks a bike at a docking station" do
+		expect(subject.release_bike.working?).to eq(true)
+	end
+
+	it "docks a bike at a docking station" do
+		expect(subject.release_bike.working?).to eq(true)
+	end
 
 	it { expect(subject).to respond_to(:release_bike)}
 
